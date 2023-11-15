2a8038cd0a746178bde8081efc6feb64f2a60784
sgandhi60 <sbgandhi@us.ibm.com>
2023-11-15 13:43:40 -0800

    First commit

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "278de435-2d0a-41c0-bd41-6e7905c7deb3";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "b0274f7b-27c7-49d7-8dcf-04bbda70709e";
+}
